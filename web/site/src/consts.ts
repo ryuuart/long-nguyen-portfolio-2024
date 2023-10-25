dce172c2d21796ebe615771496d67b0e10bc2ca8
Long Nguyen <me@long-nguyen.dev>
2023-10-25 13:22:46 -0500

    feat(site): add footer

--- web/site/src/consts.ts
+++ web/site/src/consts.ts
@@ -3,3 +3,39 @@
 
 export const SITE_TITLE = "Long Nguyen";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const FOOTER_LIST_LINKS = {
+  title: "Links",
+  links: [
+    {
+      displayText: "Portfolio",
+      url: "",
+    },
+    {
+      displayText: "Artwork",
+      url: "",
+    },
+    {
+      displayText: "Blog",
+      url: "",
+    },
+  ],
+};
+
+export const FOOTER_LIST_SOCIALS = {
+  title: "Socials",
+  links: [
+    {
+      displayText: "Github",
+      url: "",
+    },
+    {
+      displayText: "LinkedIn",
+      url: "",
+    },
+    {
+      displayText: "Instagram",
+      url: "",
+    },
+  ],
+};
