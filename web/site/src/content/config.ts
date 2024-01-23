03df8fc0058bc37d9e261d97c011d57cd8717412
Long Nguyen <me@long-nguyen.dev>
2024-01-22 18:57:14 -0600

    feat(site): migrate to use content collections

--- web/site/src/content/config.ts
+++ web/site/src/content/config.ts
@@ -1,15 +1,30 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const portfolio = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    role: z.string(),
+    link: z.object({
+      displayText: z.string(),
+      url: z.string().url(),
+    }),
+    startDate: z.coerce.date(),
+    endDate: z.union([z.coerce.date(), z.literal("present")]),
+    heroImage: z.string(),
+  }),
+});
+
+export const collections = { portfolio };
---
+++ web/site/src/content/portfolio/manawave.md
@@ -0,0 +1,13 @@
+---
+title: "MANAWAVE"
+description: "An open-source controllable, configurable, omnidirectional marquee, and design experiment."
+role: "Designer / Engineer"
+heroImage: ""
+startDate: 2022-11-00
+endDate: present
+link:
+  displayText: "Github"
+  url: "https://github.com/ryuuart/manawave"
+---
+
+asfd
